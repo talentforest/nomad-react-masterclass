026841c16a4e89e81a35375e3e3347520f3194b8
talentforest <talentforest0501@gmail.com>
2022-04-07 18:27:20 +0900

    typescript forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Circle } from './components/Circle';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState("");
+
+  // any타입 지양해야 함
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { currentTarget: { value } } = event;
+    setValue(value);
+  }
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  }
   return (
-    <div>
-      <Circle bgColor='teal' borderColor='red' />
-      <Circle bgColor='royalblue' />
-    </div>
+    <form onSubmit={onSubmit}>
+      <input
+        value={value}
+        onChange={onChange}
+        type="text"
+        placeholder="username" />
+      <button>Log in</button>
+    </form>
   );
 }
 
