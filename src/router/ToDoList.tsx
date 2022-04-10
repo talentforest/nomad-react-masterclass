3e422495e2e743111603fb3c39697a8867a6d200
talentforest <talentforest0501@gmail.com>
2022-04-10 21:02:29 +0900

    apply react-hook-form

--- src/router/ToDoList.tsx
+++ src/router/ToDoList.tsx
@@ -1,5 +1,18 @@
+import { useForm } from "react-hook-form";
+
 export default function ToDoList() {
+  const { register, watch, handleSubmit } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  }
   return (
-    <div>ToDoList</div>
+    <form onSubmit={handleSubmit(onValid)}>
+      <input {...register("email", { required: true })} placeholder="Email" />
+      <input {...register("firstName", { required: true })} placeholder="First Name" />
+      <input {...register("lastName", { required: true })} placeholder="Last Name" />
+      <input {...register("username", { required: true })} placeholder="Username" />
+      <input {...register("password", { required: true, minLength: 10 })} placeholder="Password" />
+      <button>Add</button>
+    </form>
   )
 }
