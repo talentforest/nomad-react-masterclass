abdf3a2848dc6bca6a51850044a97f2d049474c2
talentforest <talentforest0501@gmail.com>
2022-04-11 17:46:24 +0900

    ToDoList react-hook-form

--- src/router/ToDoList.tsx
+++ src/router/ToDoList.tsx
@@ -1,77 +1,29 @@
 import { useForm } from "react-hook-form";
 
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string;
 }
 
 export default function ToDoList() {
-  const { register, handleSubmit, formState:{errors}, setError } = useForm<IForm>();
-  const onValid = (data: IForm) => {
-    if(data.password !== data.password1) {
-      setError(
-        "password1", 
-        { message: "Password are not the same."},
-        { shouldFocus: true }
-      )
-    }
-    setError("extraError", {message: "Server offline."})
-  }
-  console.log(errors)
+  const { 
+    register, 
+    handleSubmit, 
+    setValue,
+  } = useForm<IForm>();
+
+  const onSubmit = (data: IForm) => {
+    console.log("add to do:", data.toDo)
+    setValue("toDo", "");
+  };
+
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <input
-        {...register("email", {
-          required: "Email is required",
-          pattern: {
-            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-            message: "Only naver email is allowed."
-          }
+        {...register("toDo", {
+          required: "Please write a To Do.",
         })}
-        placeholder="Email" />
-      <span>{errors?.email?.message}</span>
-      <input 
-        {...register("firstName", 
-          { required: "write here", 
-            validate: {
-              // 객체 리터럴을 만들수도 있다.
-              noNico: (value) => 
-                value.includes("nico") ? "no nico" : true,
-            }
-        })} 
-        placeholder="First Name" />
-      <input 
-        {...register("lastName", { 
-          required: true 
-          })
-        } 
-        placeholder="Last Name" />
-      <input {...register("username", { 
-        required: true 
-        })} 
-        placeholder="Username" />
-      <input
-        {...register("password", {
-          required: "Passwordddd is required",
-          minLength: 10
-        })}
-        placeholder="Password" />
-      <span>{errors?.password?.message}</span>
-      <input
-        {...register("password1", {
-          required: "Password is required",
-          minLength: {
-            value: 10,
-            message: "Your password is too short.",
-          }
-        })}
-        placeholder="Password" />
-        <span>{errors?.password1?.message}</span>
+        placeholder="Write a to do" 
+      />
       <button>Add</button>
     </form>
   )
