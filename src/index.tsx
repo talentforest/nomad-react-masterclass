ff91713ff1b903f3f542becc5798b129bd2b8e80
talentforest <talentforest0501@gmail.com>
2022-05-18 14:22:18 +0900

    Delete codes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./theme/theme";
+import { lightTheme } from "./theme/theme";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={lightTheme}>
         <App />
       </ThemeProvider>
     </RecoilRoot>
